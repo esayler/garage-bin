5e402668e9d9b5e97eff11aa43040b2f1ee1ec9b
Eric Sayler <easayler@gmail.com>
2017-04-13 15:27:46 -0600

    add item counts

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -32,10 +32,40 @@ class List extends Component {
       )
     }) : null
 
+    const numSparkling = list.reduce((acc, curr, index) => {
+      if (curr.cleanliness === 'Sparkling') {
+        return acc + 1
+      } else {
+        return acc
+      }
+    }, 0)
+
+    const numDusty = list.reduce((acc, curr, index) => {
+      if (curr.cleanliness === 'Dusty') {
+        return acc + 1
+      } else {
+        return acc
+      }
+    }, 0)
+   
+    const numRancid = list.reduce((acc, curr, index) => {
+      if (curr.cleanliness === 'Rancid') {
+        return acc + 1
+      } else {
+        return acc
+      }
+    }, 0)
+
     return (
       <div className='list'>
         <button className='btn btn-close' onClick={this.closeGarage}>Close Garage</button>
         <FormContainer />
+        <div className='stats'>
+          <span>Total: {list ? list.length : 0} </span>
+          <span>Sparkling: {numSparkling} </span>
+          <span>Dusty: {numDusty} </span>
+          <span>Rancid: {numRancid} </span>
+        </div>
         <div className='list-items'>
           {listItems}
         </div>
